620feda2a8068001219d31588a5bfce50750accf
tjdds1109 <tjdds1109@naver.com>
2019-03-04 21:39:46 +0900

    Commit 관리자 페이지

--- src/main/java/com/springboot/web/WebApplication.java
+++ src/main/java/com/springboot/web/WebApplication.java
@@ -2,8 +2,7 @@ package com.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
+
 
 @SpringBootApplication
 public class WebApplication {
@@ -12,14 +11,14 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
-    /**
-     * HiddenHttpMethodFilter
-     */
-    @Bean
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
-        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
-        return filter;
-    }
+//    /**
+//     * HiddenHttpMethodFilter
+//     */
+//    @Bean
+//    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+//        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+//        return filter;
+//    }
 
 }
 
